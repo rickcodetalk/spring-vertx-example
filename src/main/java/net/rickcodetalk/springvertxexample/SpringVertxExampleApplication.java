630d4e5283738e77f72df3fb71d1f460fa38fbc2
ricklee <ricklee@bc.holdings>
2019-01-30 15:57:45 +0800

    Springboot skeleton

---
+++ src/main/java/net/rickcodetalk/springvertxexample/SpringVertxExampleApplication.java
@@ -0,0 +1,14 @@
+package net.rickcodetalk.springvertxexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVertxExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringVertxExampleApplication.class, args);
+	}
+
+}
+
